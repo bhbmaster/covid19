3fada9ab8a185a75f1e204550f410235b2b75eda
bhbmaster <bhbmaster@gmail.com>
2021-02-25 14:49:31 -0800

    california: now showing 4 subplots - daily new cases,deaths,total cases and deaths PER 100K. the daily plots are 7 day moving averages

--- usa-ca/county-plot.py
+++ usa-ca/county-plot.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.offline.offline
-# from plotly.subplots import make_subplots
+from plotly.subplots import make_subplots
 
 ### INIT ###
 
@@ -16,6 +16,7 @@ url_data="https://data.ca.gov/dataset/590188d5-8545-4c93-a9a0-e230f0db7290/resou
 file_pop="county-pop.csv" # values from around 2020 good enough
 
 c=pd.read_csv(url_data)
+print(c.describe())
 
 cpops = pd.read_csv(file_pop,index_col="Rank")
 # cpops = cpops.head(15)
@@ -54,22 +55,51 @@ def avgN(N,x,y):
 
 # * graph
 def graph():
-    x=c[c.county == county]["date"].values
-    y=c[c.county == county]["newcountconfirmed"].values / pop * PER
-    avgx,avgy=avgN(ndays,x.tolist(),y.tolist())
-    print(f"* {county} pop={pop} last_x_value={avgx[-1]} last_{ndays}_day_avg_y_value={avgy[-1]:0.2f}")
-    fig.update_layout(title=f"California Counties Daily New Cases Per {PER:,} ({ndays} day Moving Average)")
+    print(f"* {county} pop={pop} - last recoded values below:")
     visible1 = "legendonly" if not county in visible_counties else None
-    # legendtext=f"<b>{county}</b> ({pop:,}) <b>{avgy[-1]:0.2f}</b> new cases per {PER_TEXT} on {avgx[-1]}"
+    x=c[c.county == county]["date"].values
+    FRONTSPACE="    "
+    # -- newcountconfirmed per 100K (moving average) -- #
+    orgy=c[c.county == county]["newcountconfirmed"].values
+    y=orgy/pop*PER
+    avgx,avgy=avgN(ndays,x.tolist(),y.tolist())
+    print(f"{FRONTSPACE}newcountconfirmed   \t x = {avgx[-1]} \t org_y = {orgy[-1]:0.0f} \t {ndays}day_avg_y_per{PER_TEXT} = {avgy[-1]:0.2f}")
     legendtext=f"<b>{county}</b> (pop {pop:,}) is <b>{avgy[-1]:0.2f}</b> on {avgx[-1]}"
-    # fig.add_trace(go.Scatter(x=avgx, y=avgy, name=legendtext, showlegend=True,visible=visible1),row=1,col=1)
-    fig.add_trace(go.Scatter(x=avgx, y=avgy, name=legendtext, showlegend=True,visible=visible1))
+    fig.add_trace(go.Scatter(x=avgx, y=avgy, name=legendtext, showlegend=True,legendgroup=county,visible=visible1),row=1,col=1)
+    # -- newcountdeaths per 100K (moving average) -- #
+    orgy=c[c.county == county]["newcountdeaths"].values
+    y=orgy/pop*PER
+    avgx,avgy=avgN(ndays,x.tolist(),y.tolist())
+    print(f"{FRONTSPACE}newcountdeaths      \t x = {avgx[-1]} \t org_y = {orgy[-1]:0.0f} \t {ndays}day_avg_y_per{PER_TEXT} = {avgy[-1]:0.2f}")
+    legendtext=f"<b>{county}</b> (pop {pop:,}) is <b>{avgy[-1]:0.2f}</b> on {avgx[-1]}"
+    fig.add_trace(go.Scatter(x=avgx, y=avgy, name=legendtext, showlegend=False,legendgroup=county,visible=visible1),row=2,col=1)
+    # -- total cases per 100K -- #
+    orgy=c[c.county == county]["totalcountconfirmed"].values
+    y=orgy/pop*PER
+    print(f"{FRONTSPACE}totalcountconfirmed \t x = {x[-1]} \t org_y = {orgy[-1]:0.0f} \t y_per{PER_TEXT} = {y[-1]:0.2f}")
+    legendtext=f"<b>{county}</b> (pop {pop:,}) is <b>{y[-1]:0.2f}</b> on {x[-1]}"
+    fig.add_trace(go.Scatter(x=x, y=y, name=legendtext, showlegend=False,legendgroup=county,visible=visible1),row=3,col=1)
+    # -- total deaths per 100K -- #
+    orgy=c[c.county == county]["totalcountdeaths"].values 
+    y=orgy/pop*PER
+    print(f"{FRONTSPACE}totalcountdeaths    \t x = {x[-1]} \t org_y = {orgy[-1]:0.0f} \t y_per{PER_TEXT} = {y[-1]:0.2f}")
+    legendtext=f"<b>{county}</b> (pop {pop:,}) is <b>{y[-1]:0.2f}</b> on {x[-1]}"
+    fig.add_trace(go.Scatter(x=x, y=y, name=legendtext, showlegend=False,legendgroup=county,visible=visible1),row=4,col=1)
+
 
 ### MAIN ###
 
 # * plotly init
-# fig = make_subplots(rows=1, cols=1)
-fig = go.Figure() 
+print("- plotting start")
+subplot_titles = (f"Daily New Cases per {PER_TEXT} {ndays}-day Moving Average",
+                  f"Daily New Deaths per {PER_TEXT} {ndays}-day Moving Average",
+                  f"Total Cases per {PER_TEXT}",
+                  f"Total Deaths per {PER_TEXT}")
+fig = make_subplots(rows=4, cols=1, shared_xaxes=True, subplot_titles=subplot_titles) # shared_xaxes to maintain zoom on all
+random_county = cpops_county_list[0] # we picked top one which is LA (most populous at the top)
+last_x = c[c.county == random_county]["date"].values.tolist()[-1]
+fig.update_layout(title=f"California Counties Covid Stats (Last Update: {last_x})")
+# fig = go.Figure() # then graph like this: fig.add_trace(go.Scatter(x=avgx, y=avgy, name=legendtext, showlegend=True,visible=visible1))
 
 # * consider each county and trace it on plotly
 for county,pop in cpop_list:
@@ -81,5 +111,6 @@ fig.write_html(output_html,auto_open=False)
 # * html div generation (not used)
 div = plotly.offline.offline.plot(fig, show_link=False, include_plotlyjs=False, output_type='div')
 print(f"len(div)={len(div)}") # div not used
+print("- plotting end")
 
 ### END
