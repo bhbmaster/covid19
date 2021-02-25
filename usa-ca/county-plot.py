d69da86269c036ea8bcb0f232a9a61fc39e7371e
bhbmaster <bhbmaster@gmail.com>
2021-02-24 18:10:10 -0800

    california changes: show top 12. prettier output. legend text shows population & last x and y value and is more clear

--- usa-ca/county-plot.py
+++ usa-ca/county-plot.py
@@ -7,7 +7,11 @@ import plotly.offline.offline
 
 ndays=7 # how many days is the moving average averaging
 output_html="county-output.html"
+PER=100000 # we should per 100000 aka 100K
+PER_TEXT="100K"
+SHOW_TOP_NUMBER=12 # how many counties to have enabled when graph shows (others can be toggled on interactively)
 
+# data input
 url_data="https://data.ca.gov/dataset/590188d5-8545-4c93-a9a0-e230f0db7290/resource/926fd08f-cc91-4828-af38-bd45de97f8c3/download/statewide_cases.csv"
 file_pop="county-pop.csv" # values from around 2020 good enough
 
@@ -15,6 +19,11 @@ c=pd.read_csv(url_data)
 
 cpops = pd.read_csv(file_pop,index_col="Rank")
 # cpops = cpops.head(15)
+
+# top 12 most populous counties
+top10 = cpops.head(SHOW_TOP_NUMBER)["County"]
+visible_counties = top10.values.tolist() # essentially its ['Los Angeles', 'San Diego', 'Orange', 'Riverside', 'San Bernardino', 'Santa Clara', 'Alameda', 'Sacramento', 'Contra Costa', 'Fresno', 'Kern', 'San Francisco']
+print(f"visible_counties={visible_counties}")
 
 ### FUNCTIONS ###
 
@@ -39,11 +48,13 @@ def avgN(N,x,y):
 # graph
 def graph():
     x=c[c.county == county]["date"].values
-    y=c[c.county == county]["newcountconfirmed"].values / pop * 100_000
+    y=c[c.county == county]["newcountconfirmed"].values / pop * PER
     avgx,avgy=avgN(ndays,x.tolist(),y.tolist())
-    print(f"{county} pop={pop} lastvalue{ndays}dayavg={avgy[-1]:0.2f}")
-    fig.update_layout(title=f"California Counties Daily New Cases ({ndays} day Moving Average)")
-    fig.add_trace(go.Scatter(x=avgx, y=avgy, name=county, showlegend=True),row=1,col=1)
+    print(f"* {county} pop={pop} lastxvalue={avgx[-1]} lastvalue{ndays}dayavg={avgy[-1]:0.2f}")
+    fig.update_layout(title=f"California Counties Daily New Cases Per {PER:,} ({ndays} day Moving Average)")
+    visible1 = "legendonly" if not county in visible_counties else None
+    legendtext=f"<b>{county}</b> ({pop:,}) <b>{avgy[-1]:0.2f}</b> new cases per {PER_TEXT} on {avgx[-1]}"
+    fig.add_trace(go.Scatter(x=avgx, y=avgy, name=legendtext, showlegend=True,visible=visible1),row=1,col=1)
 
 ### MAIN ###
 
@@ -57,4 +68,4 @@ for county,pop in zip(cpops["County"].values.tolist(),cpops["Population"].values
 # plotly
 fig.write_html(output_html,auto_open=False)
 div = plotly.offline.offline.plot(fig, show_link=False, include_plotlyjs=False, output_type='div')
-print(div)
+print(f"len(div)={len(div)}")
