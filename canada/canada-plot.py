b925d3f69f136512d68d26c84ee3ff1b5daa719e
bhbmaster <bhbmaster@gmail.com>
2021-04-20 23:05:59 -0600

    Canada: started canada plots. setup skeleton and found dataset source (see canada/canada-plot.py) also created a local population csv with 2021 canada population (estimates of course)

---
+++ canada/canada-plot.py
@@ -0,0 +1,31 @@
+#### init #####
+
+# dataset site: https://github.com/ccodwg/Covid19Canada
+# link: https://github.com/ccodwg/Covid19Canada/blob/master/timeseries_prov/active_timeseries_prov.csv
+
+# --- get data and manipulate it into correct form --- #
+
+covid_url='https://github.com/ccodwg/Covid19Canada/blob/master/timeseries_prov/active_timeseries_prov.csv' # github
+population_file='canada-pop.csv' # local
+
+# --- output names --- #
+covid_rx='canada.csv'
+covid_final='canada-parsable.csv'
+covid_html_normal='canada-output.html' # relative plots
+covid_html_raw='canada-output-raw.html'
+
+##### downloading and manipulating dataframe #####
+
+# download
+# save original
+# check
+# manipulate
+# save final
+
+##### plot #####
+
+# init plots / both figures
+# init settings for both figures (settings for plots and subplots)
+# parse each area/province and generate trace in figure
+# save html
+# the end
