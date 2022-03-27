c2b56d9482117cd248d25788ef95ea40b8e821a1
bhbmaster <bhbmaster@gmail.com>
2022-03-26 20:11:00 -0700

    prettied up clean-logs-for-places.sh comments and output

--- clean-logs-for-places.sh
+++ clean-logs-for-places.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-# run in covid19 root directory
-# run -c option to clean otherwise just shows
+# what this does: cleans stale/bad output files, run*out, this only needs to look for stale files in covid19 dir and not any of the subdirs. this is important for parse.sh to work, as it might choke on these bad output files
+# where to run: run in covid19 root directory
+# how to run: run without options to see which files are stale, and run with -c to delete them as well
+# ./clean-logs-for-places.sh     # just shows the stale output files if they exist
+# ./clean-logs-for-places.sh -c  # shows and deletes stale files
 
 EXTRA_CMD=""
 
+echo "* Stale/bad output file finder. Looks for stale or bad run.*out files in covid19 root directory; not recursively"
+
 if [[ $1 == '-c' ]]; then
    EXTRA_CMD='echo "Deleting $i"; rm -vf "$i";'
-   echo "Performing cleaning operation"
+   echo "* Performing cleaning operation"
 else
-   echo "Dry run. Not cleaning. To clean add -c argument."
+   echo "* Dry run. Not cleaning. To clean add -c argument."
 fi
 
 for i in run*out; do
     grep -q -- "- end" $i && { 
-        echo "Found at $i";
+        echo "Found stale output file: $i";
     } || {
-        echo "Missing at $i";
+        echo "Found stale output file: $i";
         eval "$EXTRA_CMD";
     }
 done | grep -v Found
+
+echo "* Done"
 
 exit 0
